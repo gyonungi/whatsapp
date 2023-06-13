ab2362c80d97b1697133da2b545d8ce3b772bee5
gyonungi <104500843+gyonungi@users.noreply.github.com>
2023-06-13 20:56:49 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Components/Login/Login";
+import Chat from "./Components/Chat/Chat";
+import Notifications from "./Components/Notifications/Notifications";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/chat" element={<Chat />}></Route>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/not" element={<Notifications />}></Route>
+      </Routes>
     </div>
   );
 }
